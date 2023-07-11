61b9654c5a40b701990aac7f2f5af227dc400d53
Your Name <you@example.com>
2023-07-11 11:36:37 +0300

    Drawer with notification updates

--- src/pages/drawer.jsx
+++ src/pages/drawer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
@@ -8,7 +8,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { mainListItems } from './listitem';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './dashboard.css'
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import ListItem from '@mui/material/ListItem';
+import Badge from '@mui/material/Badge';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import NotificationsIcon from "@mui/icons-material/Notifications";
 const drawerWidth = 240;
 
@@ -43,11 +50,21 @@ const Content = styled('div')(({ theme }) => ({
 
 export default function DashboardDrawer() {
   const [open, setOpen] = useState(false);
-
+  const [notifications, setNotifications] = useState([]);
+ 
+  const [allRead, setAllRead] = useState(false);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
+  
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleDrawerClose = () => {
     setOpen(false);
   };
@@ -55,18 +72,52 @@ export default function DashboardDrawer() {
   const toggleDrawer = () => {
     setOpen(!open);
   };
+  const handleNotificationRead = (notificationId) => {
+    const updatedNotifications = notifications.filter((notification) => notification.id !== notificationId);
+    setNotifications(updatedNotifications);
+  };  const handleNotificationClick = () => {
+    setShowDialog(true);
+  };
 
+  const [showDialog, setShowDialog] = useState(false);
+  const handleNotification = () => {
+    setShowDialog(true);
+  };
+  const unreadNotifications = notifications.filter((notification) => !notification.read);
+  useEffect(() => {
+    // Fetch notification data from your API endpoint
+    fetch('https://64ac386b9edb4181202f4c4e.mockapi.io/notif_data')
+      .then((response) => response.json())
+      .then((data) => {
+        const notificationsWithReadProperty = data.map((notification) => ({
+          ...notification,
+          read: false, // Set the read property as false by default
+        }));
+        setNotifications(notificationsWithReadProperty);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  
+  useEffect(() => {
+    const allNotificationsRead = notifications.every((notification) => notification.read);
+    setAllRead(allNotificationsRead);
+  }, [notifications]);
   return (
     <>
       <AppBar  style={{backgroundColor:"#2f4f75"}} position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
         <Typography textAlign={'center'} className='appbar'>MAVEKO</Typography>
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }} />
-          
-          <IconButton sx={{ color: 'white' }} aria-label="open drawer" >
+          <Link to="/test">
+          <IconButton sx={{ color: 'white' }} aria-label="open notification page"  onClick={handleOpen}  >
             
-            <NotificationsIcon/>
+          <Badge badgeContent={unreadNotifications.length} color="error">
+          <NotificationsIcon />
+        </Badge>
           </IconButton>
+          </Link>
           <IconButton sx={{ color: 'white' }} edge="start" aria-label="open drawer" onClick={toggleDrawer}>
             
             <MenuIcon />  
@@ -77,10 +128,49 @@ export default function DashboardDrawer() {
       <Drawer  variant="permanent" open={open}>
         <Toolbar />
         <List component="nav">{mainListItems}</List>
+
       </Drawer>
-      <Content>
-        {/* Your main content goes here */}
-      </Content>
+      <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Notifications</DialogTitle>
+        <DialogContent>
+          {notifications.length === 0 ? (
+            <DialogContentText>All notifications are read.</DialogContentText>
+          ) : (
+            <List>
+              {notifications.map((notification) => (
+                <ListItem
+                  key={notification.id}
+                  button
+                >
+                  {/* <Checkbox 
+                    checked={notification.read} 
+                    onCha
+                    nge={() => handleNotificationRead(notification.id)}
+                  /> */}
+                 
+                  <ListItemText
+                    primary={notification.title}
+                    secondary={notification.date}
+                    secondaryTypographyProps={{ color: 'textSecondary' }}
+                  />
+                  <DialogContentText>{notification.description}</DialogContentText>
+                  <IconButton
+ sx={{margin:'5px'}}
+  aria-label="open notification"
+  onClick={() => handleNotificationRead(notification.id)}
+>
+  <VisibilityIcon />
+</IconButton>
+                </ListItem>
+              ))}
+            </List>
+          )}
+          
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
