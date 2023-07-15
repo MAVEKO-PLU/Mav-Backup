bed2763426272185a430a5042a422ca558979e44
yitethio <yitbaandu703@gmail.com>
2023-07-15 19:16:16 +0300

    i am done with tha master list table with out the flag

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import CustomerList from './pages/CustomerList.jsx';
 import Dashboard from './pages/dashboard.jsx';
 import Approval from './pages/Approval.jsx'
 import Validation from "./pages/validation.jsx";
+import MyTable from './pages/mast-lst.jsx'
 
 import Notify from "./pages/notification.jsx";
 
 function App() {
   return (
     <Router>
+      
       <Routes>
         <Route path="/supplier" element={<SupplierList />} />
-        <Route path="/master" element={<MasterList />} />
+        <Route path="/master" element={<MyTable/>} />
         <Route path="/customer" element={<CustomerList />} />
         <Route path="/dash" element={<Dashboard />} />
         <Route path="/notification" element={<Notify/>} />
