7c881e1778724fda3b07ed6ec43788ee03e9a1eb
Your Name <you@example.com>
2023-07-16 19:58:43 +0300

    prev changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CustomerList from './pages/CustomerList.jsx';
 import Dashboard from './pages/dashboard.jsx';
 import Approval from './pages/Approval.jsx'
 import Validation from "./pages/validation.jsx";
+import SupplierExcel from './pages/suppliersExcl.jsx'
 
 import Notify from "./pages/notification.jsx";
 
@@ -20,6 +21,8 @@ function App() {
         <Route path="/notification" element={<Notify/>} />
         <Route path="/approval" element={<Approval />} />
         <Route path="/validation" element={<Validation />} />
+        <Route path="/excel" element={<SupplierExcel />} />
+
        
 
         <Route path="/" element={<SignIn />} />
